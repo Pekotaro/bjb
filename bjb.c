211efeb39e0a7d764318d3b422cc548f03bbb7ea
pekotaro <sakatatjktks@msn.com>
2014-11-16 11:35:02 +0900

    変数名を変更 マジックナンバーを定数化 不要な比較を削除

--- bjb.c
+++ bjb.c
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#define MAX_E_SIZE 4
+#define LEN(array) (sizeof array / MAX_E_SIZE)
+
 int main(void) {
-    char bjb_element[4][8] = {"ﾎﾞ","ﾝ","ｼﾞｮ","ｳﾞｨ"},
-         bjb_valid[6][8] = {"ｼﾞｮ","ﾝ","ﾎﾞ","ﾝ","ｼﾞｮ","ｳﾞｨ"},
-         ret[6][8] = {"","","","","",""};
-    int  random = 0, head = 0, i = 0;
+    
+    const char elements[][MAX_E_SIZE] = {"ﾎﾞ","ﾝ","ｼﾞｮ","ｳﾞｨ"},
+         valid_result[][MAX_E_SIZE] = {"ｼﾞｮ","ﾝ","ﾎﾞ","ﾝ","ｼﾞｮ","ｳﾞｨ"};
+    char result[LEN(valid_result)][MAX_E_SIZE];
+    unsigned int random = 0, head = 0, i = 0;
     
     srand((unsigned)time(NULL));
     while(1){
-        random = rand() % 4;
-        printf("%s", bjb_element[random]);
-        memcpy(ret[(head + 5) % 6],bjb_element[random], 8);
-        if(memcmp(ret[head], bjb_valid[0], (6 - head) * 8 ) == 0
-            && ( head == 0 || memcmp(ret[0], bjb_valid[6 - head], head * 8 ) == 0)) break;
-        head = ++i % 6;
+        random = rand() % (LEN(elements));
+        printf("%s", elements[random]);
+        memcpy(result[head], elements[random], MAX_E_SIZE);
+        head = ++i % LEN(result);
+        if(memcmp(result[head], valid_result[0], (LEN(result) - head) * MAX_E_SIZE ) == 0
+           &&  memcmp(result[0], valid_result[LEN(result) - head], head * MAX_E_SIZE ) == 0) break;
     }
     printf("\r\n%d Bon Jovis\r\n", i);
     printf("＿人人人人人人人人人人人人人人＿\r\n");
