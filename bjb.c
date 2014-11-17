067eab1ce219f18ae053867aa6eba3f974375e0a
pekotaro <sakatatjktks@msn.com>
2014-11-17 21:10:40 +0900

    セパレータが無く、仕様を満たしていなかったため、セパレータを出力する処理を追加

--- bjb.c
+++ bjb.c
@@ -7,20 +7,22 @@
 
 int main(void) {
     
-    const char elements[][MAX_E_SIZE] = {"ﾎﾞ","ﾝ","ｼﾞｮ","ｳﾞｨ"},
-         valid_result[][MAX_E_SIZE] = {"ｼﾞｮ","ﾝ","ﾎﾞ","ﾝ","ｼﾞｮ","ｳﾞｨ"};
+    const char elements[][MAX_E_SIZE] = {"ﾎﾞ", "ﾝ", "ｼﾞｮ", "ｳﾞｨ"},
+         valid_result[][MAX_E_SIZE] = {"ｼﾞｮ", "ﾝ", "･", "ﾎﾞ", "ﾝ", "･", "ｼﾞｮ", "ｳﾞｨ"},
+         separator[MAX_E_SIZE] = "･",
+         *random_element;
     char result[LEN(valid_result)][MAX_E_SIZE];
     unsigned int random = 0, head = 0, i = 0;
     
     srand((unsigned)time(NULL));
     while(1){
-        random = rand() % LEN(elements);
-        printf("%s", elements[random]);
-        memcpy(result[head], elements[random], MAX_E_SIZE);
+        random_element = (i % 3 == 2) ? separator : elements[rand() % LEN(elements)];
+        printf("%s", random_element);
+        memcpy(result[head], random_element, MAX_E_SIZE);
         head = ++i % LEN(result);
         if(memcmp(result[head], valid_result[0], (LEN(result) - head) * MAX_E_SIZE ) == 0
            &&  memcmp(result[0], valid_result[LEN(result) - head], head * MAX_E_SIZE ) == 0)
-	    break;
+           break;
     }
     printf("\r\n%d Bon Jovis\r\n", i);
     printf("＿人人人人人人人人人人人人人人＿\r\n");
